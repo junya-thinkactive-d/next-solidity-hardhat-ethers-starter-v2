1eb62fb72d185797df9372f7c79346ce83934fa2
junya-thinkactive-d <develop.junya@gmail.com>
2023-01-01 14:52:35 +0900

    feat: add env

--- packages/contract/hardhat.config.ts
+++ packages/contract/hardhat.config.ts
@@ -1,9 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-contract-sizer";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
 
 const config: HardhatUserConfig = {
-  solidity:{
+  solidity: {
     version: '0.8.17',
     // settings:{
     //   optimizer: {
@@ -11,11 +11,18 @@ const config: HardhatUserConfig = {
     //     runs: 200,
     //   },
   },
+  networks: {
+    goerli: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
   typechain: {
-    outDir: "../client/src/libs/hardhat/types" && "./types", 
-    target: "ethers-v5",
+    outDir: '../client/src/libs/hardhat/types' && './types',
+    target: 'ethers-v5',
     alwaysGenerateOverloads: false,
-    externalArtifacts: ["externalArtifacts/*.json"],
+    externalArtifacts: ['externalArtifacts/*.json'],
     dontOverrideCompile: false,
   },
   paths: {
